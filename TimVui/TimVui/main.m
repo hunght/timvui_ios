b41b9cd557d76f8599e3266b53218a714d341026
Hoang The Hung <hth321@gmail.com>
2013-03-22 15:43:26 +0700

    init

---
+++ TimVui/TimVui/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TimVui
+//
+//  Created by Hoang The Hung on 3/22/13.
+//  Copyright (c) 2013 Hoang The Hung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
