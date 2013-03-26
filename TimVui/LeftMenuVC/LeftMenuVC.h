96fc80d95e8e7acf6786d0829a9910dd7d51ba2b
Hoang The Hung <hth321@gmail.com>
2013-03-26 15:49:59 +0700

    add left menu

---
+++ TimVui/LeftMenuVC/LeftMenuVC.h
@@ -0,0 +1,13 @@
+//
+//  LeftMenuVC.h
+//  TimVui
+//
+//  Created by Hoang The Hung on 3/23/13.
+//  Copyright (c) 2013 Hoang The Hung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LeftMenuVC : UITableViewController
+
+@end
