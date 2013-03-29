b55f572c4120eb9e6ddd2f4c4c68372bfbd6afc3
Hoang The Hung <hth321@gmail.com>
2013-03-29 11:23:36 +0700

    undo comit
    undo

--- TimVui/App/Extensions/AppDelegate.m
+++ TimVui/App/Extensions/AppDelegate.m
@@ -19,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     
-    //ß
+    
     NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:4 * 1024 * 1024 diskCapacity:20 * 1024 * 1024 diskPath:nil];
     [NSURLCache setSharedURLCache:URLCache];
     
