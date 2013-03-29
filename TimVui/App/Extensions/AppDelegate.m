66d253b4327da195a55aaba151015e2ada10c8d9
Hoang The Hung <hth321@gmail.com>
2013-03-29 11:25:33 +0700

    THIS OK NOW

--- TimVui/App/Extensions/AppDelegate.m
+++ TimVui/App/Extensions/AppDelegate.m
@@ -18,8 +18,7 @@
 @synthesize imageController = _imageController;
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    
-    
+
     NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:4 * 1024 * 1024 diskCapacity:20 * 1024 * 1024 diskPath:nil];
     [NSURLCache setSharedURLCache:URLCache];
     
