c95e14b3f0239d32dcfb5df221611a73a23093a2
Hoang The Hung <hth321@gmail.com>
2013-04-05 15:38:17 +0700

    fix menu

--- TimVui/App/ViewControllers/LeftMenuVC/LeftMenuVC.h
+++ TimVui/App/ViewControllers/LeftMenuVC/LeftMenuVC.h
@@ -1,13 +1,21 @@
 //
-//  ExampleNestedTablesViewController.h
-//  SDNestedTablesExample
+//  DropDownExample.h
+//  VPPLibraries
 //
-//  Created by Daniele De Matteis on 27/06/2012.
-//  Copyright (c) 2012 Daniele De Matteis. All rights reserved.
+//  Created by Víctor on 13/12/11.
+//  Copyright (c) 2011 Víctor Pena Placer. All rights reserved.
 //
 
-#import "SDNestedTableViewController.h"
+#import <UIKit/UIKit.h>
+#import "VPPDropDown.h"
+#import "VPPDropDownDelegate.h"
 
-@interface LeftMenuVC : SDNestedTableViewController
+@interface DropDownExample : UITableViewController <VPPDropDownDelegate, UIActionSheetDelegate> {
+@private
+    NSArray *_headers;
+    VPPDropDown *_dropDownCustom;
+    
+    NSIndexPath *_ipToDeselect;
+}
 
 @end
--- TimVui/App/ViewControllers/LeftMenuVC/LeftMenuVC.m
+++ TimVui/App/ViewControllers/LeftMenuVC/LeftMenuVC.m
@@ -1,95 +1,409 @@
 //
-//  ExampleNestedTablesViewController.m
-//  SDNestedTablesExample
+//  DropDownExample.m
+//  VPPLibraries
 //
-//  Created by Daniele De Matteis on 27/06/2012.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Created by Víctor on 13/12/11.
+//  Copyright (c) 2011 Víctor Pena Placer. All rights reserved.
 //
 
-#import "LeftMenuVC.h"
+#import "DropDownExample.h"
+#import "GHMenuCell.h"
+#import <QuartzCore/QuartzCore.h>
+#define kNumberOfSections 3
 
-@interface LeftMenuVC ()
+enum {
+    kSection1UserAccount = 0,
+    kSection2Services,
+    kSection3Setting
+};
 
-@end
 
-@implementation LeftMenuVC
+// including the dropdown cell !!
+/* set to 3 if you want to see how it behaves 
+ when having more cells in the same section 
+ */
+#define kNumberOfRowsInSection1 2 
 
-- (id) init
+enum {
+    kS1Row0 = 0,
+    kS1Row1
+};
+
+
+/* set to 2 if you want to see how it behaves 
+ when having more cells in the same section 
+ */
+
+#define kNumberOfRowsInSection2 4
+enum {
+    kS2Row0 = 0,
+    kS2Row1,
+    kS2Row2,
+    kS2Row3
+};
+
+#define kNumberOfRowsInSection3 3
+enum {
+    kS3Row0 = 0,
+    kS3Row1,
+    kS3Row2
+};
+@implementation DropDownExample
+
+- (id)initWithStyle:(UITableViewStyle)style
 {
-    if (self = [super initWithNibName:@"SDNestedTableView" bundle:nil])
-    {
-        // do init stuff
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+
+        NSMutableArray *elts = [NSMutableArray array];
+        for (int i = 1; i <= 4; i++) {
+            // just some mock elements
+            VPPDropDownElement *e = [[VPPDropDownElement alloc] initWithTitle:[NSString stringWithFormat:@"Element %d",i] andObject:[NSNumber numberWithInt:i]];
+            [elts addObject:e];
+            [e release];
+        }
+        
+        _dropDownCustom = [[VPPDropDown alloc] initWithTitle:@"Custom Combo" 
+                                                       type:VPPDropDownTypeCustom 
+                                                  tableView:self.tableView 
+                                                  indexPath:[NSIndexPath indexPathForRow:kS1Row0 inSection:kSection1UserAccount]
+                                                   elements:elts 
+                                                   delegate:self];
+        _headers=[[NSArray alloc] initWithObjects:@"Tài khoản",@"Từ Anuong.net",@"Cài đặt", nil];
+        [self.tableView setBackgroundColor:[UIColor clearColor]];
+        [self.view setBackgroundColor:[UIColor clearColor]];
+        UIColor *bgColor = [UIColor colorWithRed:(50.0f/255.0f) green:(57.0f/255.0f) blue:(74.0f/255.0f) alpha:1.0f];
+        self.tableView.backgroundColor = bgColor;
+        self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+        self.view.backgroundColor = bgColor;
     }
     return self;
 }
 
-#pragma mark - Nested Tables methods
-
-- (NSInteger)mainTable:(UITableView *)mainTable numberOfItemsInSection:(NSInteger)section
+- (void) dealloc {
+    
+    if (_ipToDeselect != nil) {
+        [_ipToDeselect release];
+        _ipToDeselect  = nil;
+    }
+    if (_dropDownCustom != nil) {
+        [_dropDownCustom release];
+        _dropDownCustom = nil;
+    }
+    
+    [super dealloc];
+}
+- (void)didReceiveMemoryWarning
 {
-    return 8;
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
 }
 
-- (NSInteger)mainTable:(UITableView *)mainTable numberOfSubItemsforItem:(SDGroupCell *)item atIndexPath:(NSIndexPath *)indexPath
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
 {
-    return 3; 
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
-- (SDGroupCell *)mainTable:(UITableView *)mainTable setItem:(SDGroupCell *)item forRowAtIndexPath:(NSIndexPath *)indexPath
+- (void)viewDidUnload
 {
-    item.itemText.text = [NSString stringWithFormat:@"My Main Item %u", indexPath.row +1];
-    return item;
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
 }
 
-- (SDSubCell *)item:(SDGroupCell *)item setSubItem:(SDSubCell *)subItem forRowAtIndexPath:(NSIndexPath *)indexPath
+- (void)viewWillAppear:(BOOL)animated
 {
-    subItem.itemText.text = [NSString stringWithFormat:@"My Sub Item %u", indexPath.row +1];
-    return subItem;
+    [super viewWillAppear:animated];
 }
 
-- (void) mainTable:(UITableView *)mainTable itemDidChange:(SDGroupCell *)item
+- (void)viewDidAppear:(BOOL)animated
 {
-    SelectableCellState state = item.selectableCellState;
-    NSIndexPath *indexPath = [self.tableView indexPathForCell:item];
-    switch (state) {
-        case Checked:
-            NSLog(@"Changed Item at indexPath:%@ to state \"Checked\"", indexPath);
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Table view data source
+
+- (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    
+    return 44;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return kNumberOfSections;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    int rows = [VPPDropDown tableView:tableView numberOfExpandedRowsInSection:section];
+    switch (section) {
+
+        case kSection1UserAccount:
+            rows += kNumberOfRowsInSection1;
             break;
-        case Unchecked:
-            NSLog(@"Changed Item at indexPath:%@ to state \"Unchecked\"", indexPath);
+        case kSection2Services:
+            rows += kNumberOfRowsInSection2;
             break;
-        case Halfchecked:
-            NSLog(@"Changed Item at indexPath:%@ to state \"Halfchecked\"", indexPath);
+        case kSection3Setting:
+            rows += kNumberOfRowsInSection3;
             break;
-        default:
+            
+    }
+    return rows;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    
+    GHMenuCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[[GHMenuCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+    }
+    
+    
+    // Configure the cell...
+    cell.textLabel.text = nil;
+
+    if ([VPPDropDown tableView:tableView dropdownsContainIndexPath:indexPath]) {
+        return [VPPDropDown tableView:tableView cellForRowAtIndexPath:indexPath];
+    }
+    
+    // first check if any dropdown contains the requested cell
+    int row = indexPath.row - [VPPDropDown tableView:tableView numberOfExpandedRowsInSection:indexPath.section];
+    switch (indexPath.section) {
+        case kSection1UserAccount:
+            switch (row) {
+                case kS2Row1:
+                    cell.textLabel.text = @"This is an independent cell";
+                    break;
+            }
             break;
+        case kSection2Services:
+            switch (row) {
+                case kS1Row0:
+                    cell.textLabel.text = @"This is an independent cell";
+                    break;
+            }
+            break;
+
+    }
+        
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Table view delegate
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+	return (_headers[section] == [NSNull null]) ? 0.0f : 21.0f;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+	NSObject *headerText = _headers[section];
+	UIView *headerView = nil;
+	if (headerText != [NSNull null]) {
+		headerView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, [UIScreen mainScreen].bounds.size.height, 21.0f)];
+		CAGradientLayer *gradient = [CAGradientLayer layer];
+		gradient.frame = headerView.bounds;
+		gradient.colors = @[
+                      (id)[UIColor colorWithRed:(67.0f/255.0f) green:(74.0f/255.0f) blue:(94.0f/255.0f) alpha:1.0f].CGColor,
+                      (id)[UIColor colorWithRed:(57.0f/255.0f) green:(64.0f/255.0f) blue:(82.0f/255.0f) alpha:1.0f].CGColor,
+                      ];
+		[headerView.layer insertSublayer:gradient atIndex:0];
+		
+		UILabel *textLabel = [[UILabel alloc] initWithFrame:CGRectInset(headerView.bounds, 12.0f, 5.0f)];
+		textLabel.text = (NSString *) headerText;
+		textLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:([UIFont systemFontSize] * 0.8f)];
+		textLabel.shadowOffset = CGSizeMake(0.0f, 1.0f);
+		textLabel.shadowColor = [UIColor colorWithWhite:0.0f alpha:0.25f];
+		textLabel.textColor = [UIColor colorWithRed:(125.0f/255.0f) green:(129.0f/255.0f) blue:(146.0f/255.0f) alpha:1.0f];
+		textLabel.backgroundColor = [UIColor clearColor];
+		[headerView addSubview:textLabel];
+		
+		UIView *topLine = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, [UIScreen mainScreen].bounds.size.height, 1.0f)];
+		topLine.backgroundColor = [UIColor colorWithRed:(78.0f/255.0f) green:(86.0f/255.0f) blue:(103.0f/255.0f) alpha:1.0f];
+		[headerView addSubview:topLine];
+		
+		UIView *bottomLine = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 21.0f, [UIScreen mainScreen].bounds.size.height, 1.0f)];
+		bottomLine.backgroundColor = [UIColor colorWithRed:(36.0f/255.0f) green:(42.0f/255.0f) blue:(5.0f/255.0f) alpha:1.0f];
+		[headerView addSubview:bottomLine];
+	}
+	return headerView;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     [detailViewController release];
+     */
+
+    
+    // first check if any dropdown contains the requested cell
+    if ([VPPDropDown tableView:tableView dropdownsContainIndexPath:indexPath]) {
+        [VPPDropDown tableView:tableView didSelectRowAtIndexPath:indexPath];
+        return;
+    }
+    
+    int row = indexPath.row - [VPPDropDown tableView:tableView numberOfExpandedRowsInSection:indexPath.section];
+    UIAlertView *av;
+    switch (indexPath.section) {
+        case kSection2Services:
+            switch (row) {
+                case kS1Row1:
+                    av = [[UIAlertView alloc] initWithTitle:@"Cell selected" message:@"The independent cell 1 has been selected" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                    [av show];
+                    [av release];
+                    //[tableView deselectRowAtIndexPath:indexPath animated:YES];
+                    break;
+                    
+                default:
+                    break;
+            }
+            break;
+            
+        case kSection1UserAccount:
+            switch (row) {
+                case kS2Row1:
+                    av = [[UIAlertView alloc] initWithTitle:@"Cell selected" message:@"The independent cell 2 has been selected" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                    [av show];
+                    [av release];
+                    //[tableView deselectRowAtIndexPath:indexPath animated:YES];
+                    break;
+                    
+                default:
+                    break;
+            }
+            
+            break;
+    }
+    
+}
+
+
+#pragma mark - VPPDropDownDelegate
+
+- (void) dropDown:(VPPDropDown *)dropDown elementSelected:(VPPDropDownElement *)element atGlobalIndexPath:(NSIndexPath *)indexPath {
+    
+    if (dropDown == _dropDownCustom) {
+        //[self.tableView deselectRowAtIndexPath:indexPath animated:YES];
     }
 }
 
-- (void) item:(SDGroupCell *)item subItemDidChange:(SDSelectableCell *)subItem
-{
-    SelectableCellState state = subItem.selectableCellState;
-    NSIndexPath *indexPath = [item.subTable indexPathForCell:subItem];
-    switch (state) {
-        case Checked:
-            NSLog(@"Changed Sub Item at indexPath:%@ to state \"Checked\"", indexPath);
-            break;
-        case Unchecked:
-            NSLog(@"Changed Sub Item at indexPath:%@ to state \"Unchecked\"", indexPath);
-            break;
-        default:
-            break;
+- (UITableViewCell *) dropDown:(VPPDropDown *)dropDown rootCellAtGlobalIndexPath:(NSIndexPath *)globalIndexPath {
+    static NSString *cellIdentifier = @"GlobalDropDownCell";
+    
+    GHMenuCell *cell = [self.tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (!cell) {
+        cell = [[[GHMenuCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cellIdentifier] autorelease];
     }
+    
+    cell.textLabel.text = @"Đăng nhập";
+    
+    return cell;
 }
 
-- (void)expandingItem:(SDGroupCell *)item withIndexPath:(NSIndexPath *)indexPath
-{
-    NSLog(@"Expanded Item at indexPath: %@", indexPath);
+- (UITableViewCell *) dropDown:(VPPDropDown *)dropDown cellForElement:(VPPDropDownElement *)element atGlobalIndexPath:(NSIndexPath *)globalIndexPath {
+    static NSString *cellIdentifier = @"CustomDropDownCell";
+
+    GHMenuCell *cell = [self.tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (!cell) {
+        cell = [[[GHMenuCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cellIdentifier] autorelease];
+    }
+    
+    cell.textLabel.text = @"Custom cell";
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"row %d",globalIndexPath.row];
+    
+    return cell;
 }
 
-- (void)collapsingItem:(SDGroupCell *)item withIndexPath:(NSIndexPath *)indexPath 
-{
-    NSLog(@"Collapsed Item at indexPath: %@", indexPath);
+
+- (CGFloat) dropDown:(VPPDropDown *)dropDown heightForElement:(VPPDropDownElement *)element atIndexPath:(NSIndexPath *)indexPath {
+    float height = dropDown.tableView.rowHeight;
+    
+    return height + indexPath.row * 10;
+}
+
+
+#pragma mark - UIAlertViewDelegate
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (_ipToDeselect != nil) {
+        [self.tableView deselectRowAtIndexPath:_ipToDeselect animated:YES];
+        [_ipToDeselect release];
+        _ipToDeselect = nil;
+    }
 }
 
 @end
